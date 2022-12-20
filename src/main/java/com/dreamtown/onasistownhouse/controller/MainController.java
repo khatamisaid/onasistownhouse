f4f40e8e970939e6074b4a825749a0d24f74d638
said <khatamisaid@gmail.com>
2022-12-20 16:40:07 +0700

    menambahkan filemanager

--- src/main/java/com/dreamtown/onasistownhouse/controller/MainController.java
+++ src/main/java/com/dreamtown/onasistownhouse/controller/MainController.java
@@ -1,9 +1,17 @@
 package com.dreamtown.onasistownhouse.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.dreamtown.onasistownhouse.utils.FileManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -12,8 +20,18 @@ public class MainController {
     @Autowired
     private HttpSession httpSession; 
 
+    @Autowired
+    private FileManager fileManager; 
+
     @GetMapping(value = "/")
     public String index(Model model) {
         return "index";
+    }
+
+    @GetMapping(value = "/ftp")
+    public ResponseEntity<Map> ftp(Model model) {
+        Map data = new HashMap<>();
+        data.put("isConnected", fileManager.connected());
+        return new ResponseEntity<>(data, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/dreamtown/onasistownhouse/utils/FileManager.java
@@ -0,0 +1,84 @@
+package com.dreamtown.onasistownhouse.utils;
+
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.servlet.http.HttpSession;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileManager {
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private HttpSession httpSession;
+
+    private ChannelSftp setupJsch() throws JSchException {
+        JSch jsch = new JSch();
+        jsch.setKnownHosts("C:\\Users\\MTJ 03\\.ssh\\known_hosts");
+        // jsch.setKnownHosts("C:\\Users\\Administrator\\.ssh\\known_hosts");
+        // jsch.setKnownHosts("/.ssh/known_hosts");
+        Session jschSession;
+        if (httpSession.getAttribute(env.getProperty("ftp.username")) != null) {
+            jschSession = (Session) httpSession.getAttribute(env.getProperty("ftp.username"));
+            return (ChannelSftp) jschSession.openChannel("sftp");
+        }
+        jschSession = jsch.getSession(env.getProperty("ftp.username"), env.getProperty("ftp.remoteHost"));
+        jschSession.setPassword(env.getProperty("ftp.password"));
+        jschSession.connect();
+        httpSession.setAttribute(env.getProperty("ftp.username"), jschSession);
+        return (ChannelSftp) jschSession.openChannel("sftp");
+    }
+
+    public Boolean connected() {
+        ChannelSftp channelSftp = null;
+        try {
+            channelSftp = setupJsch();
+            return channelSftp.isConnected();
+        } catch (JSchException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public void uploadFile(MultipartFile file, String pathWithFileName) {
+        ChannelSftp channelSftp = null;
+        try {
+            channelSftp = setupJsch();
+            channelSftp.connect();
+            InputStream inputStream = new BufferedInputStream(file.getInputStream());
+            channelSftp.put(inputStream, pathWithFileName);
+            // channelSftp.exit();
+        } catch (JSchException | SftpException | IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String downloadFile(String file) {
+        ChannelSftp channelSftp = null;
+        try {
+            channelSftp = setupJsch();
+            channelSftp.connect();
+            InputStream is = channelSftp.get(file);
+            byte[] bytes = IOUtils.toByteArray(is);
+            String imageStr = Base64.encodeBase64String(bytes);
+            // channelSftp.exit();
+            return imageStr;
+        } catch (JSchException | IOException | SftpException e) {
+            System.out.println(e.getMessage());
+        }
+        return "";
+    }
+}
