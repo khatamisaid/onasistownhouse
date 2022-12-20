a1b0476898abc89c260ca705f0ec3a5cf03e9349
said <khatamisaid@gmail.com>
2022-12-20 20:31:25 +0700

    update

--- src/main/java/com/dreamtown/onasistownhouse/controller/MainController.java
+++ src/main/java/com/dreamtown/onasistownhouse/controller/MainController.java
@@ -3,13 +3,17 @@ package com.dreamtown.onasistownhouse.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,11 +21,16 @@ import org.springframework.web.multipart.MultipartFile;
 import com.dreamtown.onasistownhouse.utils.FileManager;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -62,9 +71,15 @@ public class MainController {
         return new ResponseEntity<>(data, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/loadfile/{filename}")
+    @RequestMapping(value = "/loadfile/{filename}", method = RequestMethod.GET)
     @ResponseBody
-    public FileSystemResource loadfile(@PathVariable String filename) {
-        return new FileSystemResource(new File(env.getProperty("storage") + filename));
+    public ResponseEntity<InputStreamResource> loadfile(@PathVariable String filename) throws MalformedURLException, IOException {
+        File fileTemp = new File(env.getProperty("storage") + filename);
+        URLConnection connection = fileTemp.toURL().openConnection();
+        String mimeType = connection.getContentType();
+        InputStream is = new FileInputStream(fileTemp);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(mimeType))
+                .body(new InputStreamResource(is));
     }
 }
