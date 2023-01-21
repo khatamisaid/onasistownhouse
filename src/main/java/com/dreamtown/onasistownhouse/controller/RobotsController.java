5d0f5891c635fd7278b8ed73a73a36de9a12e429
khatamisaid <khatamisaid@gmail.com>
2023-01-22 02:08:36 +0700

    update

--- src/main/java/com/dreamtown/onasistownhouse/controller/RobotsController.java
+++ src/main/java/com/dreamtown/onasistownhouse/controller/RobotsController.java
@@ -1,18 +1,15 @@
 package com.dreamtown.onasistownhouse.controller;
 
-import java.util.Arrays;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RobotsController {
-    @RequestMapping(value = "/robots.txt", method = RequestMethod.GET)
-    public String getRobots(HttpServletRequest request) {
-        return (Arrays.asList("https://onasistownhouse.com","onasistownhouse.com", "www.onasistownhouse.com").contains(request.getServerName())) ?
-                "robotsAllowed" : "robotsDisallowed";
+    @RequestMapping(value = { "/robots.txt", "/robot.txt" })
+    @ResponseBody
+    public String getRobotsTxt() {
+        return "User-agent: *\n" +
+                "Disallow: /admin\n";
     }
 }
